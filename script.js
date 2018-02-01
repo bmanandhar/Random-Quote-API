bef8aa35592d43a93337018aafc70b2b63d1b20d
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-02-01 06:45:36 -0800

    some alteration added

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-
 $(document).ready(function(){
 
   getQuote();
-  var quote;
-  var author;
+  var quote, author;
 
-function getQuote(){
-
+  function getQuote(){
   var url= "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
   $.getJSON(url, function(data){
+    console.log("Bijaya's codepen");
+    console.log(typeof data);
+    console.log("Type of data");
     quote = data.quoteText;
-    author = data.quoteAuthor;
-    $(".quote").html('"'+data.quoteText+'"');
-    $(".author").html("-"+data.quoteAuthor);
+    if (data.quoteAuthor) {
+      author = data.quoteAuthor;
+    } else {
+      author = "Anonymous";
+    }
+    $(".quote").html('"'+quote+'"');
+    $(".author").html("-"+author);
   });
 };
     $("#tweet").on("click", function(){
-    window.open("https://twitter.com/intent/tweet?text=" + quote + author);
+    window.open("https://twitter.com/intent/tweet?text=" + quote + "   - " + author);
   });
     $("#newQuote").on("click", function(){
     getQuote();
