8f596b830a3107262d081a012a76f84249269c5b
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-02-21 14:15:05 -0800

    some changes

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This web application has been made available to public to read and learn several
 
 * `index.html` and `style.css` combined will display a webpage having a prompting window, buttons, images and texts on the browser. 
 
-* Once the "New quote" buttom is clicked, the `script.js` will get activated and accesses the public API data library `http://forismatic.com/en/` to reneder the randomly generated data containing quote, author name, and URL of data in the cloud.
+* Once the "New quote" buttom is clicked, the `script.js` will get activated and accesses the public API data library <a href="http://forismatic.com/en/">http://forismatic.com/en/</a> to reneder the randomly generated data containing quote, author name, and URL of data in the cloud.
 
 * the data then can be sent directly to Facebook and/or Tweeter for posting, if the user wants. 
 
--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
+  //Variables declared
   var quote, author, link;
+  //Function declaration  
   function getQuote(){
   var url= "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+   
+  //API call using Json GET method
   $.getJSON(url, function(data){
+    
     console.log("Bijaya's codepen");
     console.log(typeof data);
     console.log("Type of data");
     console.log(data);
+    
     quote = data.quoteText;
     author = data.quoteAuthor;
-    link = data.quoteLink
+    link = data.quoteLink;
+    
     if (data.quoteAuthor) {
       author = data.quoteAuthor;
     } else {
@@ -20,13 +27,17 @@ $(document).ready(function(){
     $(".link").html(link);
   });
 };
+  
     $("#tweet").on("click", function(){
     window.open("https://twitter.com/intent/tweet?text="+quote+"   - "+author);
-  });
+    });
+  
     $(".facebook").on("click", function(){
     window.open("https://www.facebook.com/sharer/sharer.php?u="+ link);
   });
-    $("#newQuote").on("click", function(){
+  
+    $("#newQuote").on("click", function(){     
     getQuote();
   });
+  
 });
